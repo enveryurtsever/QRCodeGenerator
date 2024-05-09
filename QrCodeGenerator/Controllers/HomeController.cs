822ef330299b3d20791369800c502c22a0d82d8e
Enver Yurtsever <55282643+enveryurtsever@users.noreply.github.com>
2024-05-09 15:05:19 +0300

    first commit

---
+++ QrCodeGenerator/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿
+using QrCodeGenerator.Models;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QrCodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult QrqrGenerate(string qr)
+        {
+            return Json(GenerateOrDisplayQRCode(qr));
+        }
+        public string GenerateOrDisplayQRCode(string data)
+        {
+   
+
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+
+            var qrCodeData = qRCodeGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            var qrCodeImage = qrCode.GetGraphic(20);
+
+            // Convert the image to byte array
+            MemoryStream ms = new MemoryStream();
+            qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            byte[] byteImage = ms.ToArray();
+            string base64ImageRepresentation = Convert.ToBase64String(byteImage);
+            string DosyaAdi = Sabitler.RandomString(10);
+            Sabitler.SaveImage(base64ImageRepresentation, DosyaAdi);
+            // Return the image as inline content
+            return DosyaAdi;
+        }
+      
+    }
+}
---
+++ QrCodeGenerator/Models/Sabitler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace QrCodeGenerator.Models
+{
+    public static class Sabitler
+    {
+
+
+        public static bool SaveImage(string ImgStr, string ImgName)
+        {
+            String path = HttpContext.Current.Server.MapPath("~/qrCodes"); //Path
+
+            //Check if directory exist
+            if (!System.IO.Directory.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(path); //Create directory if it doesn't exist
+            }
+
+            string imageName = ImgName + ".png";
+
+            //set the image path
+            string imgPath = Path.Combine(path, imageName);
+
+            byte[] imageBytes = Convert.FromBase64String(ImgStr);
+
+            File.WriteAllBytes(imgPath, imageBytes);
+
+            return true;
+        }
+
+
+        private static Random random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
